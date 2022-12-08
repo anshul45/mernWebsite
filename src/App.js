be6b8ce1f0e5d84c031c033fb92e22b9295cf4ed
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-08 23:10:16 +0530

    Creating routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is react App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const home = () => {
+  return <div>This is Home</div>;
+};
+
+export default home;
