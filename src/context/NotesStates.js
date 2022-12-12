4f1098efa0bccdcd2e59a57d90c0b27842ea4dd6
anshul45 <80778090+anshul45@users.noreply.github.com>
2022-12-12 15:24:35 +0530

    commit

--- src/context/NotesStates.js
+++ src/context/NotesStates.js
@@ -3,7 +3,7 @@ import NoteContext from "./NoteContext";
 const NoteState = (props) => {
   const notesInitial = [
     {
-      _id: "638f4f7f0555158ec2709c0e",
+      _id: "638f4f7f0555158ec2709c03e",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -12,7 +12,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f840555158e3c2709c610",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -21,7 +21,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405535158ec2709c10s",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -30,7 +30,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405525158ec2709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -39,7 +39,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8420555158ec2709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -48,7 +48,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f840555158ec22709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -57,7 +57,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405551584ec2709c1p0",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -66,7 +66,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405551598ec2709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -75,7 +75,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405551558ec2709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -84,7 +84,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f8405551584ec2709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -93,7 +93,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f840555158ec22709c10",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -102,7 +102,7 @@ const NoteState = (props) => {
       __v: 0,
     },
     {
-      _id: "638f4f840555158ec2709c10",
+      _id: "638f4f840555158ec2709c110",
       user: "638cb0d9dc0a785871cad748",
       title: "my Title",
       description: "Please wake up early",
@@ -112,8 +112,33 @@ const NoteState = (props) => {
     },
   ];
   const [notes, setNotes] = useState(notesInitial);
+
+  //Add a Note
+  const addNote = (title, description, tag) => {
+    //To API Call
+    console.log("Adding a note");
+    const note = {
+      _id: "638f4f840555158ec2709c120",
+      user: "638cb0d9dc0a785871cad748",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2022-12-06T14:19:48.133Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+
+  //Delete a Note
+
+  const deleteNote = () => {};
+
+  //Edit a Note
+
+  const editNote = () => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
